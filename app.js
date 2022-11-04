b43a2f092d95d58ea51a6060c38ee765f4650e6d
YuginProgramming <dudaryev@gmail.com>
2022-11-04 14:58:42 +0200

    second friday

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const { eachItem } = require('ajv/dist/compile/util');
 const express = require('express');
 const upload = require('multer')();
 const server = express();
@@ -6,8 +5,6 @@ const server = express();
 const db = require('./db');
 //підключив конфіг до моделі колекції
 const AdsModel = require('./models/ads');
-const dbName = 'ads';
-//const client = new MongoClient(url);
 
 server.set('view engine', 'ejs');
 server.set('views', './views');
@@ -16,18 +13,10 @@ server.use(express.static('./static'));
 server.use(express.json());
 
 server.get('/', (req, res) => {
-    //const db = client.db(dbName);
-    const collection = db.collection('ads');
-    //find some documents
-    collection.find({});
-    //assert.equal(err, null);
-    //const vars = { content: AdsModel.fill };
-    res.render('main', {'ads': eachItem});
+    res.render('main');
 });
     
 server.post('/test', upload.none(), async (req, res) => {
-  //console.log('post');
-  //let { data } = req.body.Name;
   // зробить стрілочну 
   async function writeData (data) {
     console.log('live');
@@ -43,7 +32,6 @@ server.post('/test', upload.none(), async (req, res) => {
   const result = await readData();
   console.log(result)
   //res.send(result)
-  
 }); 
 
 const PORT = process.env.PORT || 3000;
